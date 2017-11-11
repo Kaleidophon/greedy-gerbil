e3c6ee92fb9dda71b2e7770b33e01550d60a3778
Kaleidophon <Dennis.Ulmer@gmx.de>
2017-11-11 18:53:30 +0100

    Wrote code to get question / answer one hot vectors.

--- prep.py
+++ prep.py
@@ -4,9 +4,13 @@ Preparing data for later experiments.
 """
 
 # STD
+from collections import defaultdict
 import json
 import gzip
 import os
+
+# EXT
+import numpy as np
 
 # CONST
 DATA_SET_TYPES = ("test", "train", "valid")
@@ -57,6 +61,30 @@ class Answer:
         return not self.__eq__(other)
 
 
+class HotLookup:
+    """
+    Class to look up hot one vectors given a vocabulary.
+    """
+    def __init__(self, vocabulary, key_func=lambda key: key, entry2index=None):
+        self.vocabulary = vocabulary
+        self.key_func = key_func
+        self.entry2index = entry2index
+
+    def __getitem__(self, item):
+        key = self.key_func(item)
+
+        # Convert to indices if necessary
+        if self.entry2index is not None:
+            if type(key) == list:
+                key = [self.entry2index[el] for el in key]
+            else:
+                key = [self.entry2index[key]]
+
+        key = set(key) if type(key) == list else {key}
+
+        return np.array([1 if entry in key else 0 for entry in self.vocabulary])
+
+
 def get_data_set(set_name, unique_answers=False):
     """
     Retrieve all the question and their respective answers from a data set (possible options are
@@ -65,7 +93,7 @@ def get_data_set(set_name, unique_answers=False):
     :param unique_answers: Flag to indicate whether duplicate answers to a question should be kept.
     :type unique_answers: bool
     """
-    questions = dict()
+    questions, answers = dict(), dict()
 
     assert set_name in DATA_SET_TYPES, "Expected set name to be in {}, '{}' found.".format(DATA_SET_TYPES, set_name)
     raw_questions = read_data_file(path=DATA_SET_PATH.format("questions", set_name))["questions"]
@@ -90,16 +118,20 @@ def get_data_set(set_name, unique_answers=False):
 
         # Parse answers and add to question
         for raw_answer in annotation["answers"]:
-            answer_id = raw_answer["answer_id"]
+            choice_number = raw_answer["answer_id"]
+            answer_id = "{}/{}".format(question_id, choice_number)
             answer = raw_answer["answer"]
 
+            answer_obj = Answer(
+                uid=answer_id, image_id=question.image_id, answer=answer,
+                confidence=raw_answer["answer_confidence"], atype=answer_type
+            )
             if answer not in question or not unique_answers:
-                question.answers[answer_id] = Answer(
-                    uid="{}/{}".format(question_id, answer_id), image_id=question.image_id, answer=answer,
-                    confidence=raw_answer["answer_confidence"], atype=answer_type
-                )
+                question.answers[choice_number] = answer_obj
 
-    return questions
+            answers[answer_id] = answer_obj
+
+    return questions, answers
 
 
 def read_data_file(path):
@@ -110,9 +142,67 @@ def read_data_file(path):
         return json.loads(file.read())
 
 
-if __name__ == "__main__":
-    for _, question in get_data_set("valid", unique_answers=True).items():
-        print(question)
-        for answer in question:
-            print(answer)
+def get_vocabulary(source, entry_getter, threshold=0, index_vocab=True, add_unk=True):
+    """
+    Generate vocabulary from a collection.
 
+    :param source: Source collection for vocabulary.
+    :param entry_getter: Function to get a entry for the vocabulary from an item in the source collection.
+    :param threshold: Number of times a term has to appear to be included in the vocabulary.
+    :param index_vocab: Index the words in the vocabulary.
+    :param add_unk: Add entry for unknowns.
+    """
+    frequencies = defaultdict(int)
+
+    # Count frequencies
+    for item in source:
+        entries = entry_getter(item)
+        if type(entries) == list:
+            for entry in entries:
+                frequencies[entry] += 1
+        else:
+            entry = entries
+            frequencies[entry] += 1
+
+    # Filter according to threshold
+    vocabulary = {key for key, value in frequencies.items() if value >= threshold}
+    if add_unk:
+        vocabulary.add("<unk>")
+
+    # Index entries
+    entry2index = {entry: index for index, entry in enumerate(vocabulary)}
+    index2entry = {index: entry for index, entry in enumerate(vocabulary)}
+
+    # Convert vocab
+    if index_vocab:
+        vocabulary = [entry2index[entry] for entry in vocabulary]
+
+    return vocabulary, entry2index, index2entry
+
+
+if __name__ == "__main__":
+    questions, answers = get_data_set("train", unique_answers=False)
+
+    #for _, question in questions.items():
+    #    print(question)
+    #    for answer in question:
+    #        print(answer)
+
+    question_vocabulary, qe2i, qi2e = get_vocabulary(
+        questions.values(), entry_getter=lambda question: question.question.replace("?", "").split(" "),
+        index_vocab=True, threshold=1000
+    )
+    answer_vocabulary, ae2i, ai2e = get_vocabulary(
+        list(answers.values()), entry_getter=lambda answer: answer.answer, index_vocab=False, add_unk=False,
+        threshold=1000
+    )
+
+    # print(answer_vocabulary)
+    # hots_answers = HotLookup(answer_vocabulary)
+    # print(hots_answers["red"])
+
+    print(question_vocabulary)
+    hots_questions = HotLookup(
+        question_vocabulary, key_func=lambda key: key.replace("?", "").split(" "), entry2index=qe2i
+    )
+    print(hots_questions["How many are you?"])
