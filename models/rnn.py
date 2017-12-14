67ee35398df79bff70fb912203ffc33f203a4924
Kuroo <dkuric20@gmail.com>
2017-12-14 17:41:12 +0100

    RNN working!!!

--- models/rnn.py
+++ models/rnn.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 from torch.autograd import Variable
 
 class RNNModel(nn.Module):
-    def __init__(self, vocab_size, image_size, hidden_size, output_size, num_layers=1, cuda_enabled=False):
+    def __init__(self, vocab_size, image_size, hidden_size, output_size, num_layers=1, dropout_prob=0, cuda_enabled=False):
         super(RNNModel, self).__init__()
         # init properties
         self.num_layers = num_layers
@@ -12,7 +12,7 @@ class RNNModel(nn.Module):
         self.cuda_enabled = cuda_enabled
         # init network structure
         self.embedding = nn.Embedding(vocab_size + 1, hidden_size, padding_idx=vocab_size)
-        self.gru = nn.GRU(hidden_size, hidden_size, num_layers)
+        self.gru = nn.GRU(hidden_size, hidden_size, num_layers, dropout=dropout_prob, batch_first=True)
         self.layer_transform = nn.Linear(image_size + hidden_size, output_size)
         #self.softmax = nn.Softmax(dim=1)
         # self.softmax = nn.LogSoftmax(dim=1)
@@ -30,12 +30,13 @@ class RNNModel(nn.Module):
         gru_states = gru_states if gru_states else self._initialize_gru_states(batch_size)
         # embed questions
         embeddings = self.embedding(questions)
-        # resize to seq_len, batch_size, hidden_size
-        embeddings = embeddings.view(questions.size(1), batch_size, self.hidden_size)
         # pass everything to GRU
         encodings, gru_states = self.gru(embeddings, gru_states)
         # get last states by selecting state at question's last non-padding index
-        last_states = torch.index_select(encodings, 0, question_lengths)
+        # question_lengths = question_lengths.view(batch_size,1).expand(batch_size, self.hidden_size)
+
+        # Nobody understands whats happening in this line but it works for some reason thanks to google (just skip it)
+        last_states = encodings.gather(1, question_lengths.view(-1, 1, 1).expand(encodings.size(0), 1, encodings.size(2))).view(batch_size, self.hidden_size)
         #last_states = encodings.index_select(0, question_lengths)
         # predict answers
         question_vectors = torch.cat((last_states, images), 1)
